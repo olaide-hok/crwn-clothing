1d0ce8104b036069bdb2ebd261121f7830c460d8
olaide-hok <olaidekareem43@yahoo.com>
2021-10-21 17:53:52 +0100

    implemented firebase utils, including ability to store authenticated users into our firestore database.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component.jsx';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 
@@ -18,14 +18,29 @@ class App extends React.Component {
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
-  componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+  unsubscribeFromAuth = null;
 
-      console.log(user);
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapShot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });        
+      }
+      
+       this.setState({ currentUser: userAuth});
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,13 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+//import firebase from 'firebase/app';
+
+//import firebase from 'firebase/compat/app';
+
+//import 'firebase/firestore';
+//import 'firebase/auth';
+
+
+import firebase from 'firebase';
+require('firebase/auth');
 
 const config = {
     apiKey: "AIzaSyA3XbaUACaoq9-LOcybNWyJFrgSj0e6ePg",
@@ -9,6 +16,32 @@ const config = {
     storageBucket: "crwn-db-a3f08.appspot.com",
     messagingSenderId: "1073550954532",
     appId: "1:1073550954532:web:1ca97bf6ce9f6419ef08b1"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    
+    return userRef;
 };
 
 firebase.initializeApp(config);
